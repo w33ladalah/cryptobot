ae200bf9e3e07bd3ace097f8a344f255f5c7792c
Hendro Wibowo <hendrothemail@gmail.com>
2025-02-25 10:29:12 +0700

    Update manage_services.sh

--- scripts/manage_services.sh
+++ scripts/manage_services.sh
@@ -2,4 +2,7 @@
 
 COMPOSE_FILE="${COMPOSE_FILE:-./docker/run-development-compose.yaml}"
 
-docker compose -f "${COMPOSE_FILE}" up
+docker compose -f "${COMPOSE_FILE}" down
+docker rmi -f cryptobot-development-worker
+docker compose -f "${COMPOSE_FILE}" up -d
+docker compose -f "${COMPOSE_FILE}" logs -f
